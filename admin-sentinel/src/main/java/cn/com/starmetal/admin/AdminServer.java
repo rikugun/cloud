808bfb772e1758017c2cb08476d1907f78947f46
rikugun <v5.rikugun@gmail.com>
2019-07-05 17:25:39 +0800

    fix: add nacos support

--- admin-sentinel/src/main/java/cn/com/starmetal/admin/AdminServer.java
+++ admin-sentinel/src/main/java/cn/com/starmetal/admin/AdminServer.java
@@ -13,10 +13,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAdminServer
-public class AppRunner {
+public class AdminServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppRunner.class, args);
+        SpringApplication.run(AdminServer.class, args);
     }
 
 
